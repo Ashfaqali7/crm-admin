8b625d0d29f37c24d70cb0db968cdf5e64b20395
Ashfaq Ali <Ashfaq.Ali@bssuniversal.com>
2025-10-03 11:17:37 +0500

    banner message

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,37 +1,48 @@
 import { createContext, useContext, useState, type ReactNode } from 'react';
-import { ConfigProvider, theme } from 'antd';
+import { ConfigProvider, message, theme } from 'antd';
 
 interface ThemeContextType {
   isDarkMode: boolean;
   toggleTheme: () => void;
-}
+  showBanner: (content: string, type: MessageType) => void;
+};
+type MessageType = "success" | "info" | "warning" | "error" | "loading";
 
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [messageApi, contextHolder] = message.useMessage();
 
-  const toggleTheme = () => {
-    setIsDarkMode(!isDarkMode);
+  const toggleTheme = () => setIsDarkMode(!isDarkMode);
+
+  // Success helper
+  const showBanner = (content: string, type: MessageType = "success") => {
+    messageApi.open({
+      type: type,
+      content,
+    });
   };
 
   const themeConfig = {
     algorithm: isDarkMode ? theme.darkAlgorithm : theme.defaultAlgorithm,
     token: {
-      colorPrimary: '#1C6EA4', // main brand blue
-      colorLink: '#33A1E0', // links / hover
-      colorSuccess: '#154D71', // success / accents
-      colorWarning: '#FFF9AF', // warnings / highlights
+      colorPrimary: '#1C6EA4',
+      colorLink: '#33A1E0',
+      colorSuccess: '#154D71',
+      colorWarning: '#FFF9AF',
       colorError: '#E03131',
-      colorTextBase: isDarkMode ? '#FFFFFF' : '#000000', colorBgBase: isDarkMode ? '#000000' : '#FFFFFF',
+      colorTextBase: isDarkMode ? '#FFFFFF' : '#000000',
+      colorBgBase: isDarkMode ? '#000000' : '#FFFFFF',
       colorBorder: '#54565781',
       colorBorderSecondary: '#54565781',
     },
   };
 
   return (
-    <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+    <ThemeContext.Provider value={{ isDarkMode, toggleTheme, showBanner, }}>
       <ConfigProvider theme={themeConfig}>
+        {contextHolder}
         {children}
       </ConfigProvider>
     </ThemeContext.Provider>
